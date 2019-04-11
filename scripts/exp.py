4ea01e24f8055a7ecaec847ffbbcd16d93920b71
master8 <master8.michael@gmail.com>
2019-04-11 22:44:02 +0500

    тест на 20K

--- scripts/exp.py
+++ scripts/exp.py
@@ -2,30 +2,41 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import AffinityPropagation
 
-data = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/df_vacancies_full_w2v_d2_ru.pkl')
+data = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/df_vacancies_full_w2v_ru.pkl')
 
-n = 50000
-is_run = True
-while is_run:
-    try:
-        print(n)
-        co = data[data.is_prog == False].sample(n)
+co = data[data.is_prog].sample(20000)
 
-        X = np.array(co['w2v_d2'])
-        X = X.tolist()
+X = np.array(co['w2v'])
+X = X.tolist()
 
-        from sklearn.metrics.pairwise import cosine_distances
-        word_cosine = cosine_distances(X)
+labels = AffinityPropagation().fit_predict(X)
 
-        labels = AffinityPropagation(affinity='precomputed').fit_predict(word_cosine)
+co['label'] = labels
 
-        co['label'] = labels
+co[['id', 'label']].to_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_result_20K_prog.csv', index=False)
 
-        co[['id', 'label']].to_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_clusters_results_ru_other.csv', index=False)
-        is_run = False
-    except:
-        n = n - 10000
-        is_run = True
+# n = 50000
+# is_run = True
+# while is_run:
+#     try:
+#         print(n)
+#         co = data[data.is_prog].sample(n)
+#
+#         X = np.array(co['w2v_d2'])
+#         X = X.tolist()
+#
+#         from sklearn.metrics.pairwise import cosine_distances
+#         word_cosine = cosine_distances(X)
+#
+#         labels = AffinityPropagation(affinity='precomputed').fit_predict(word_cosine)
+#
+#         co['label'] = labels
+#
+#         co[['id', 'label']].to_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_clusters_results_ru_other.csv', index=False)
+#         is_run = False
+#     except:
+#         n = n - 10000
+#         is_run = True
 
 
 # co = data[data.is_prog == False]

645b90df44399ba89dec55ae564aa37f545951a0
master8 <master8.michael@gmail.com>
2019-04-14 02:12:15 +0500

    exp k param and save results

--- scripts/exp_k_param.py
+++ scripts/exp_k_param.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import KMeans
 
 from sklearn.metrics.cluster import adjusted_rand_score
 from sklearn.metrics.cluster import homogeneity_completeness_v_measure
@@ -11,25 +11,23 @@ data = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/c
 vectors_name = 'w2v_300'
 
 co = data[data.is_prog]
-co_train = co[co.is_train]
+
+co_train = co[co.is_train].sample(4000)
+co_test = co[co.is_test]
+
+X_train = np.array(co_train[vectors_name])
+X_train = X_train.tolist()
+
+X_test = np.array(co_test[vectors_name])
+X_test = X_test.tolist()
 
 count = []
 ars = []
 hcv = []
 
-for i in range(10000, 999, -500):
-    print('train size ' + str(i))
-    co = co.sample(i)
-    co_train = co_train.sample(i)
-    co_test = co[co.is_test]
-
-    X_train = np.array(co_train[vectors_name])
-    X_train = X_train.tolist()
-
-    X_test = np.array(co_test[vectors_name])
-    X_test = X_test.tolist()
-
-    model = AffinityPropagation()
+for i in range(10, 41, 1):
+    print('k = ' + str(i))
+    model = KMeans(n_clusters=i, n_jobs=-1)
     model.fit(X_train)
     labels = model.predict(X_test)
     co_test['label_test'] = labels
@@ -48,7 +46,7 @@ df['count'] = count
 df['ars'] = ars
 
 df.columns = ['homogeneity', 'completeness', 'v_measure', 'count', 'ars']
-df['size'] = range(10000, 999, -500)
+df['k'] = range(10, 41, 1)
 
-co = df[['size', 'count', 'ars', 'homogeneity', 'completeness', 'v_measure']]
-co.to_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_clusters_results_ru_prog_size.csv', index=False)
+co = df[['k', 'count', 'ars', 'homogeneity', 'completeness', 'v_measure']]
+co.to_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_clusters_results_ru_prog_k.csv', index=False)

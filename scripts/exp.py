e49f60079ab89a6a9048b14448620b4c776a26fa
master8 <master8.michael@gmail.com>
2019-04-17 14:13:47 +0500

    exp and results

--- scripts/exp.py
+++ scripts/exp.py
@@ -15,42 +15,16 @@ from util import clustering
 # for k in range(40, 101, 10):
 #     clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans_' + str(k))
 
-k = 16
-
+k = 200
+#
 clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans')
-clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1), 'HDBSCAN')
+# clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, alpha=.5), 'HDBSCAN')
 clustering.cluster(AgglomerativeClustering(n_clusters=k), 'Agglomerative')
 clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1), 'SpectralClustering')
-clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1, assign_labels='discretize'), 'SpectralClustering')
-clustering.cluster(AffinityPropagation(), 'AffinityPropagation')
-clustering.cluster(Birch(n_clusters=k), 'Birch')
+# clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1, assign_labels='discretize'), 'SpectralClustering')
+# clustering.cluster(AffinityPropagation(), 'AffinityPropagation')
+clustering.cluster(Birch(n_clusters=KMeans(n_clusters=16, n_jobs=-1)), 'Birch')
 clustering.cluster_fit_pred(GaussianMixture(n_components=k), 'GaussianMixture')
 
-metrics = {'braycurtis': hdbscan.dist_metrics.BrayCurtisDistance,
- 'canberra': hdbscan.dist_metrics.CanberraDistance,
- 'chebyshev': hdbscan.dist_metrics.ChebyshevDistance,
- 'cityblock': hdbscan.dist_metrics.ManhattanDistance,
- 'dice': hdbscan.dist_metrics.DiceDistance,
- 'euclidean': hdbscan.dist_metrics.EuclideanDistance,
- 'hamming': hdbscan.dist_metrics.HammingDistance,
- 'haversine': hdbscan.dist_metrics.HaversineDistance,
- 'infinity': hdbscan.dist_metrics.ChebyshevDistance,
- 'jaccard': hdbscan.dist_metrics.JaccardDistance,
- 'kulsinski': hdbscan.dist_metrics.KulsinskiDistance,
- 'l1': hdbscan.dist_metrics.ManhattanDistance,
- 'l2': hdbscan.dist_metrics.EuclideanDistance,
- 'mahalanobis': hdbscan.dist_metrics.MahalanobisDistance,
- 'manhattan': hdbscan.dist_metrics.ManhattanDistance,
- 'matching': hdbscan.dist_metrics.MatchingDistance,
- 'minkowski': hdbscan.dist_metrics.MinkowskiDistance,
- 'p': hdbscan.dist_metrics.MinkowskiDistance,
- 'pyfunc': hdbscan.dist_metrics.PyFuncDistance,
- 'rogerstanimoto': hdbscan.dist_metrics.RogersTanimotoDistance,
- 'russellrao': hdbscan.dist_metrics.RussellRaoDistance,
- 'seuclidean': hdbscan.dist_metrics.SEuclideanDistance,
- 'sokalmichener': hdbscan.dist_metrics.SokalMichenerDistance,
- 'sokalsneath': hdbscan.dist_metrics.SokalSneathDistance,
- 'wminkowski': hdbscan.dist_metrics.WMinkowskiDistance}
-
-for m in ['chebyshev', 'euclidean', 'hamming', 'jaccard', 'manhattan']:
-    clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, metric=metrics[m]), 'HDBSCAN')
+# for d in range(1, 10, 1):
+#     clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, alpha=d/10), 'HDBSCAN')

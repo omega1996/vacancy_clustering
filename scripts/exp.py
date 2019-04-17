0d30f7349d7ee6d190c7811b4c3969db27e4d3db
master8 <master8.michael@gmail.com>
2019-04-17 13:04:33 +0500

    exp and results

--- scripts/exp.py
+++ scripts/exp.py
@@ -25,3 +25,32 @@ clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1, assign_labels='di
 clustering.cluster(AffinityPropagation(), 'AffinityPropagation')
 clustering.cluster(Birch(n_clusters=k), 'Birch')
 clustering.cluster_fit_pred(GaussianMixture(n_components=k), 'GaussianMixture')
+
+metrics = {'braycurtis': hdbscan.dist_metrics.BrayCurtisDistance,
+ 'canberra': hdbscan.dist_metrics.CanberraDistance,
+ 'chebyshev': hdbscan.dist_metrics.ChebyshevDistance,
+ 'cityblock': hdbscan.dist_metrics.ManhattanDistance,
+ 'dice': hdbscan.dist_metrics.DiceDistance,
+ 'euclidean': hdbscan.dist_metrics.EuclideanDistance,
+ 'hamming': hdbscan.dist_metrics.HammingDistance,
+ 'haversine': hdbscan.dist_metrics.HaversineDistance,
+ 'infinity': hdbscan.dist_metrics.ChebyshevDistance,
+ 'jaccard': hdbscan.dist_metrics.JaccardDistance,
+ 'kulsinski': hdbscan.dist_metrics.KulsinskiDistance,
+ 'l1': hdbscan.dist_metrics.ManhattanDistance,
+ 'l2': hdbscan.dist_metrics.EuclideanDistance,
+ 'mahalanobis': hdbscan.dist_metrics.MahalanobisDistance,
+ 'manhattan': hdbscan.dist_metrics.ManhattanDistance,
+ 'matching': hdbscan.dist_metrics.MatchingDistance,
+ 'minkowski': hdbscan.dist_metrics.MinkowskiDistance,
+ 'p': hdbscan.dist_metrics.MinkowskiDistance,
+ 'pyfunc': hdbscan.dist_metrics.PyFuncDistance,
+ 'rogerstanimoto': hdbscan.dist_metrics.RogersTanimotoDistance,
+ 'russellrao': hdbscan.dist_metrics.RussellRaoDistance,
+ 'seuclidean': hdbscan.dist_metrics.SEuclideanDistance,
+ 'sokalmichener': hdbscan.dist_metrics.SokalMichenerDistance,
+ 'sokalsneath': hdbscan.dist_metrics.SokalSneathDistance,
+ 'wminkowski': hdbscan.dist_metrics.WMinkowskiDistance}
+
+for m in ['chebyshev', 'euclidean', 'hamming', 'jaccard', 'manhattan']:
+    clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, metric=metrics[m]), 'HDBSCAN')

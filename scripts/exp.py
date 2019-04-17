51da16c098545f08837702d07c93e11a11ad91d1
master8 <master8.michael@gmail.com>
2019-04-17 12:07:22 +0500

    exp and results

--- scripts/exp.py
+++ scripts/exp.py
@@ -3,7 +3,7 @@ from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import AffinityPropagation
 from sklearn.cluster import SpectralClustering
 from sklearn.mixture import GaussianMixture
-# from sklearn.cluster import Birch
+from sklearn.cluster import Birch
 from sklearn.cluster import KMeans
 
 from util import clustering
@@ -15,11 +15,11 @@ from util import clustering
 # for k in range(40, 101, 10):
 #     clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans_' + str(k))
 
-# labels = hdbscan.HDBSCAN(core_dist_n_jobs=-1).fit_predict(X)
-# labels = AgglomerativeClustering(n_clusters=20).fit_predict(X)
-# labels = AffinityPropagation().fit_predict(X)
-# labels = SpectralClustering(n_clusters=20, n_jobs=-1).fit_predict(X)
-# model = GaussianMixture(n_components=20).fit(X)
-# labels = model.predict(X)
-# labels = Birch(n_clusters=20).fit_predict(X)
-# labels = KMeans(n_clusters=20, n_jobs=-1).fit_predict(X)
+clustering.cluster(KMeans(n_clusters=20, n_jobs=-1), 'SpectralClustering')
+clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1), 'HDBSCAN')
+clustering.cluster(AgglomerativeClustering(n_clusters=20), 'Agglomerative')
+clustering.cluster(SpectralClustering(n_clusters=20, n_jobs=-1), 'SpectralClustering')
+clustering.cluster(SpectralClustering(n_clusters=20, n_jobs=-1, assign_labels='discretize'), 'SpectralClustering')
+clustering.cluster(AffinityPropagation(), 'AffinityPropagation')
+clustering.cluster(Birch(n_clusters=20), 'Birch')
+clustering.cluster_fit_pred(GaussianMixture(n_components=20), 'GaussianMixture')
--- util/clustering.py
+++ util/clustering.py
@@ -53,3 +53,51 @@ def cluster(model, cname):
     pd.concat([co, df]).to_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_clusters_results_ru_13K_prog.csv', index=False)
 
 
+
+
+def cluster_fit_pred(model, cname):
+    re = []
+
+    print('=====================================')
+    print(cname)
+
+    for name in ['w2v', 'tfidf', 'elmo', 'fasttext']:
+
+        m = clone(model)
+
+        data = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_13K_' + name + '.pkl')
+        vectors_name = str(name) + '_300'
+        co = data[data.is_prog]
+
+        X = np.array(co[vectors_name])
+        X = X.tolist()
+
+        m.fit(X)
+        labels = m.predict(X)
+
+        co['label_test'] = labels
+
+        print()
+        print('-----------------------------------')
+        print(name)
+
+        print('nunique: ' + str(co[co.is_test].label_test.nunique()))
+        print('adjusted_rand_score: ' + str(adjusted_rand_score(co[co.is_test].label_true, co[co.is_test].label_test)))
+        print('adjusted_mutual_info_score: ' + str(adjusted_mutual_info_score(co[co.is_test].label_true, co[co.is_test].label_test)))
+        print('homogeneity_completeness_v_measure: ' + str(homogeneity_completeness_v_measure(co[co.is_test].label_true, co[co.is_test].label_test)))
+
+        hcv = homogeneity_completeness_v_measure(co[co.is_test].label_true, co[co.is_test].label_test)
+
+        a = np.array(
+            [cname, name, co[co.is_test].label_test.nunique(),
+             adjusted_rand_score(co[co.is_test].label_true, co[co.is_test].label_test),
+             adjusted_mutual_info_score(co[co.is_test].label_true, co[co.is_test].label_test),
+             hcv[0], hcv[1], hcv[2],
+             str(model)])
+
+        re.append(a)
+
+    co = pd.read_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_clusters_results_ru_13K_prog.csv')
+    df = pd.DataFrame(re, columns=['name', 'vec', 'n', 'ARI', 'AMI', 'Homogeneity', 'completeness', 'v_measure', 'model'])
+    pd.concat([co, df]).to_csv('/home/mluser/master8_projects/clustering_vacancies/results/df_vacancies_full_clusters_results_ru_13K_prog.csv', index=False)
+

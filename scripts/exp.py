21bb9d29abc3f7a0a3b876c0a119dfdaafbf39a6
master8 <master8.michael@gmail.com>
2019-04-17 14:45:20 +0500

    exp and results

--- scripts/exp.py
+++ scripts/exp.py
@@ -15,16 +15,16 @@ from util import clustering
 # for k in range(40, 101, 10):
 #     clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans_' + str(k))
 
-k = 200
+k = 300
 #
-clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans')
+# clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans')
 # clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, alpha=.5), 'HDBSCAN')
-clustering.cluster(AgglomerativeClustering(n_clusters=k), 'Agglomerative')
-clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1), 'SpectralClustering')
+# clustering.cluster(AgglomerativeClustering(n_clusters=k), 'Agglomerative')
+# clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1), 'SpectralClustering')
 # clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1, assign_labels='discretize'), 'SpectralClustering')
 # clustering.cluster(AffinityPropagation(), 'AffinityPropagation')
-clustering.cluster(Birch(n_clusters=KMeans(n_clusters=16, n_jobs=-1)), 'Birch')
-clustering.cluster_fit_pred(GaussianMixture(n_components=k), 'GaussianMixture')
+clustering.cluster(Birch(n_clusters=None), 'Birch')
+# clustering.cluster_fit_pred(GaussianMixture(n_components=k), 'GaussianMixture')
 
 # for d in range(1, 10, 1):
 #     clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, alpha=d/10), 'HDBSCAN')

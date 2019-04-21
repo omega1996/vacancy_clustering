443029578574c81dbfa44e776fce7b058106e796
master8 <master8.michael@gmail.com>
2019-04-21 20:46:56 +0500

    reuslts full

--- trainelmo.sh
+++ trainelmo.sh
@@ -5,6 +5,7 @@ export PYTHONPATH="/home/mluser/master8_projects/clustering_vacancies/"
 cd /home/mluser/master8_projects/clustering_vacancies/elmo/
 
 nohup python -m elmoformanylangs.biLM train \
+    --gpu 0 \
     --train_path /home/mluser/master8_projects/clustering_vacancies/data/corpus/ru_vacancies.raw \
     --config_path configs/cnn_50_100_512_4096_sample.json \
     --model output/ru_vacancies \

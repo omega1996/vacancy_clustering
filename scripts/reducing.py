e71cb21556c27267740a6439d8e70dd56a4eaa99
master8 <master8.michael@gmail.com>
2019-04-27 16:08:06 +0500

    tfidf

--- scripts/reducing.py
+++ scripts/reducing.py
@@ -2,8 +2,8 @@ import pickle
 from sklearn.decomposition import TruncatedSVD
 import pandas as pd
 
-in_file = '/home/mluser/master8_projects/pycharm_project_755/data/new/vacancies_split_tfidf.pkl'
-n = 3
+in_file = '/home/mluser/master8_projects/clustering_vacancies/data/corpus/vacancies_full_ru_22K_tfidf_all.pkl'
+n = 300
 
 file = open(in_file, 'rb')
 v = pickle.load(file)
@@ -12,4 +12,8 @@ file.close()
 svd = TruncatedSVD(n_components=n).fit(v)
 x = svd.transform(v)
 
-co = pd.DataFrame(x, columns=['x', 'y', 'z'])
+co = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_22K.pkl')
+co['tfidf_' + str(n)] = x.tolist()
+co = co[['id', 'is_prog', 'is_test', 'label_true', 'title', 'tfidf_' + str(n)]]
+
+co.to_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_22K_tfidf_' + str(n) + '.pkl')
--- scripts/vectorizing_tfidf.py
+++ scripts/vectorizing_tfidf.py
@@ -4,16 +4,22 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 
-co = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_42K.pkl')
+import pickle
+
+co = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_22K.pkl')
 documents = np.array(co.lemmatized_text_pos_tags.apply(eval).apply(lambda x: ' '.join(x)))
 
 vectorizer = TfidfVectorizer(ngram_range=(1, 3), analyzer='word')
 vectors = vectorizer.fit_transform(documents)
 
-svd = TruncatedSVD(n_components=300).fit(vectors)
-vectors = svd.transform(vectors)
+file = open('/home/mluser/master8_projects/clustering_vacancies/data/corpus/vacancies_full_ru_22K_tfidf_all.pkl', 'wb')
+pickle.dump(vectors, file)
+file.close()
 
-co['tfidf_300'] = vectors.tolist()
-co = co[['id', 'is_prog', 'is_test', 'is_train', 'label_true', 'tfidf_300']]
-
-co.to_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_42K_tfidf.pkl')
+# svd = TruncatedSVD(n_components=300).fit(vectors)
+# vectors = svd.transform(vectors)
+#
+# co['tfidf_300'] = vectors.tolist()
+# co = co[['id', 'is_prog', 'is_test', 'label_true', 'title', 'tfidf_300']]
+#
+# co.to_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_22K_tfidf_300.pkl')

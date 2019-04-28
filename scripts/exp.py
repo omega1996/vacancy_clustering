47d548d46d2661f304efa09b33596aa14094de37
master8 <master8.michael@gmail.com>
2019-04-28 19:02:49 +0500

    exp

--- scripts/exp.py
+++ scripts/exp.py
@@ -2,12 +2,21 @@
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.cluster import AffinityPropagation
 from sklearn.cluster import SpectralClustering
-from sklearn.mixture import GaussianMixture
+# from sklearn.mixture import GaussianMixture
 from sklearn.cluster import Birch
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 
 from util import clustering
 
+k = 38
+
+clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans')
+clustering.cluster(AgglomerativeClustering(n_clusters=k), 'Agglomerative')
+clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1), 'SpectralClustering')
+clustering.cluster(AffinityPropagation(), 'AffinityPropagation')
+clustering.cluster(Birch(n_clusters=None), 'Birch')
+clustering.cluster(DBSCAN(n_jobs=-1), 'DBSCAN')
 
 # for k in range(16, 31, 1):
 #     clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans_' + str(k))
@@ -15,16 +24,10 @@ from util import clustering
 # for k in range(40, 101, 10):
 #     clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans_' + str(k))
 
-k = 38
-#
-clustering.cluster(KMeans(n_clusters=k, n_jobs=-1), 'KMeans')
 # clustering.cluster_sparse(KMeans(n_clusters=k, n_jobs=-1), 'KMeans')
-# clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, alpha=.5), 'HDBSCAN')
-# clustering.cluster(AgglomerativeClustering(n_clusters=k), 'Agglomerative')
-# clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1), 'SpectralClustering')
+
 # clustering.cluster(SpectralClustering(n_clusters=k, n_jobs=-1, assign_labels='discretize'), 'SpectralClustering')
-# clustering.cluster(AffinityPropagation(), 'AffinityPropagation')
-# clustering.cluster(Birch(n_clusters=None), 'Birch')
+# clustering.cluster(hdbscan.HDBSCAN(core_dist_n_jobs=-1, alpha=.5), 'HDBSCAN')
 # clustering.cluster_fit_pred(GaussianMixture(n_components=k), 'GaussianMixture')
 
 # for d in range(1, 10, 1):

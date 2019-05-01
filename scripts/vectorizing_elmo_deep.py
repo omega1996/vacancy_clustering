910a3e49ac2bb8ec3323156639c2af0abaaed180
master8 <master8.michael@gmail.com>
2019-05-01 20:09:23 +0500

    elmo

--- scripts/vectorizing_elmo_deep.py
+++ scripts/vectorizing_elmo_deep.py
@@ -8,16 +8,16 @@ os.environ["TFHUB_CACHE_DIR"] = '/home/mluser/master8_projects/clustering_vacanc
 
 from deeppavlov.models.embedders.elmo_embedder import ELMoEmbedder
 
-elmo = ELMoEmbedder("http://files.deeppavlov.ai/deeppavlov_data/elmo_ru-twitter_2013-01_2018-04_600k_steps.tar.gz")
+elmo = ELMoEmbedder("http://files.deeppavlov.ai/deeppavlov_data/elmo_ru-wiki_600k_steps.tar.gz")
 # elmo([['вопрос', 'жизни', 'Вселенной', 'и', 'вообще', 'всего'], ['42']])
 
-co = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_22K.pkl')
-documents = np.array(co.preprocessed_text).tolist()
+co = pd.read_pickle('/home/mluser/master8_projects/clustering_vacancies/data/release/df_vacancies_full_ru_22K.pkl')
+documents = np.array(co.preprocessed_text.apply(eval)).tolist()
 
 print(str(datetime.now()))
 vectors = elmo(documents)
 print(str(datetime.now()))
 
-co['elmo_1024_twitter'] = np.array(vectors).tolist()
-co = co[['id', 'is_prog', 'is_test', 'label_true', 'title', 'elmo_1024_twitter']]
-co.to_pickle('/home/mluser/master8_projects/clustering_vacancies/data/corpus/df_vacancies_full_ru_22K_elmo_1024_twitter.pkl')
+co['elmo_1024_wiki'] = np.array(vectors).tolist()
+co = co[['id', 'elmo_1024_wiki']]
+co.to_pickle('/home/mluser/master8_projects/clustering_vacancies/data/release/df_vacancies_full_ru_22K_elmo_1024_wiki.pkl')
